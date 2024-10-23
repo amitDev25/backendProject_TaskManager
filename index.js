4446065ca5c7a82c1601b02172dabe4574a3e760
amitDev25 <amitroy251203@gmail.com>
2024-10-23 12:25:55 +0530

    Update Edit feature

--- index.js
+++ index.js
@@ -23,12 +23,44 @@ app.get("/file/:fileName", function(req, res){
     })
 });
 
+app.get("/edit/:fileName", function(req, res){
+    fs.readFile(`./files/${req.params.fileName}`, "utf-8", function(err, fileData){
+        res.render("edit", {fileName: req.params.fileName, fileData: fileData})        
+    })
+});
+
+app.post("/edit", function(req, res){
+    // console.log(req.body)
+    const prevNewName = req.body.newName;
+    const newName = prevNewName.split(" ");
+    for (let i = 0; i < newName.length; i++) {
+        if (newName[i]) { 
+            newName[i] = newName[i][0].toUpperCase() + newName[i].substr(1);
+        }
+    }
+    const newName2 = newName.join("");
+
+    fs.writeFile(`./files/${req.body.previousName}.txt`, req.body.newDetails, function(err){
+        // res.redirect("/");
+    })
+
+    fs.rename(`./files/${req.body.previousName}.txt`, `./files/${newName2}.txt`, function(err){
+        res.redirect('/')
+        
+    })
+
+    
+
+
+
+});
+
 app.post("/create", function(req, res){
     const fileTitle = req.body.title;
     const newTitle = fileTitle.split(" ");
 
     for (let i = 0; i < newTitle.length; i++) {
-        if (newTitle[i]) { // Ensure the word is not an empty string
+        if (newTitle[i]) { 
             newTitle[i] = newTitle[i][0].toUpperCase() + newTitle[i].substr(1);
         }
     }
